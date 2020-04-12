eeb46994acd132d5e13d5a8f6b90081c544f9027
Michael Vu Le <michael.vu.le96+github@gmail.com>
2020-04-12 16:35:35 -0700

    Move constants files into own folders.

--- my-app/src/components/UsStatesForm.tsx
+++ my-app/src/components/UsStatesForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import { NonTerritoryStates } from './UsStateConstants';
+import { NonTerritoryStates } from '../constants/UsStateConstants';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
