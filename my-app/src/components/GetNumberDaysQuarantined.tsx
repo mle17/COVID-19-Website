42adc7c7a551c8228ed60b8fec1219be22929a5c
Michael Vu Le <michael.vu.le96+github@gmail.com>
2020-04-11 18:22:57 -0700

    Separate components into separate files.

---
+++ my-app/src/components/GetNumberDaysQuarantined.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+export function GetNumberDaysQuarantined() {
+  return (<TextField id="standard-number" label="Day(s)" type="number" InputLabelProps={{
+    shrink: true,
+  }} />);
+}
---
+++ my-app/src/components/UsStateSelect.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { NonTerritoryStates } from './UsStateConstants';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    option: {
+      fontSize: 15,
+      '& > span': {
+        marginRight: 10,
+        fontSize: 18,
+      },
+    },
+  });
+
+export default function UsStateSelect() {
+  const classes = useStyles();
+  return (<Autocomplete id="country-select-demo" style={{ width: 300 }} options={NonTerritoryStates} classes={{
+    option: classes.option,
+  }} autoHighlight getOptionLabel={(option) => option.name} renderOption={(option) => (<React.Fragment>
+    <span>{option.abbreviation}</span>
+    {option.name}
+  </React.Fragment>)} renderInput={(params) => (<TextField {...params} label="Choose a state" variant="outlined" inputProps={{
+    ...params.inputProps,
+    autoComplete: 'new-password',
+  }} />)} />);
+}
