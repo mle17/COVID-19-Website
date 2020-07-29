5201bed656c7ee5927adbdb5dafca85b0894ee56
Michael Le <michael.vu.le96@gmail.com>
2020-07-29 12:43:50 -0700

    Add container width for app.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import Covid19App from './components/Covid19App';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Covid19App />
+        <Container maxWidth="sm">
+          <Covid19App />
+        </Container>
       </header>
     </div>
   );
